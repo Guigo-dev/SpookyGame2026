7e904d67faa875e784f48502697777c7a81482c4
Guigo-dev <guipadiferreira@gmail.com>
2026-09-20 00:10:25 -0300

    primeira movimentacao

---
+++ Scenes/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@export var sprite_2d: AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
+	
+	if direction.x < 0:
+		sprite_2d.flip_h = true
+	elif direction.x > 0:
+		sprite_2d.flip_h = false
+	
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+	
+	if velocity != Vector2.ZERO:
+		sprite_2d.play("walk")
+	else:
+		sprite_2d.play("idle")
+
+	move_and_slide()
