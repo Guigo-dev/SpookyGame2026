1e431d2ee064257f82f46cea9a9eec9f4c367789
AndreLuisGrein <andreluisgrein@gmail.com>
2026-09-21 20:46:08 -0300

    Liga/desliga lanterna / Ativador  teste sala de estar

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
+@onready var flashlight: PointLight2D = $Flashlight
+var flashlight_timer = 0
+var test_1 = false
+var inside_room = false
 
 const SPEED = 150.0
 @export var sprite_2d: AnimatedSprite2D
 
 func _ready() -> void:
 	add_to_group("player")
+	
+func _input(event):
+	if event.is_action_pressed("flashlight"):
+		flashlight.enabled = !flashlight.enabled 
 
 func _physics_process(delta: float) -> void:
 
@@ -27,3 +35,22 @@ func _physics_process(delta: float) -> void:
 		sprite_2d.play("idle")
 
 	move_and_slide()
+	
+
+	#lanterna dentro da sala
+	if inside_room and not flashlight.enabled and not test_1:
+		flashlight_timer += delta
+
+		if flashlight_timer >= 10:
+			test_1 = true
+
+
+func _on_living_room_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		inside_room = true
+
+
+func _on_living_room_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		inside_room = false
+		flashlight_timer = 0
