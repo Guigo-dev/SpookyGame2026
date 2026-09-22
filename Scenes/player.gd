63d6ccbe12ba1b7fc33b08dfd9cede8f672a9495
AndreLuisGrein <andreluisgrein@gmail.com>
2026-09-21 22:52:24 -0300

    fix - Teste só ativa se ficar 10 segundos seguidos

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -44,6 +44,9 @@ func _physics_process(delta: float) -> void:
 		if flashlight_timer >= 10:
 			test_1 = true
 			$"../Living_room/LivingRoomLight".enabled = true
+			
+	elif inside_room and flashlight.enabled and not test_1:
+		flashlight_timer = 0
 
 
 func _on_living_room_body_entered(body: Node2D) -> void:
