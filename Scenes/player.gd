3f9fb93d8fe32595073127aa06c47e8d71168269
AndreLuisGrein <andreluisgrein@gmail.com>
2026-09-22 01:14:07 -0300

    Hitbox Da interação do personagem

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 @onready var flashlight: PointLight2D = $Flashlight
 var flashlight_timer = 0
 var test_1 = false
+var test_2 = false
+var test_3 = false 
 var inside_room = false
 
 const SPEED = 150.0
 @export var sprite_2d: AnimatedSprite2D
+var direction_looking := Vector2.DOWN
 
 func _ready() -> void:
 	add_to_group("player")
@@ -28,14 +31,25 @@ func _physics_process(delta: float) -> void:
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		
+		
 	
 	if velocity != Vector2.ZERO:
 		sprite_2d.play("walk")
 	else:
 		sprite_2d.play("idle")
-
-	move_and_slide()
+		
+		
+	#Hitbox da interação
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			direction_looking = Vector2(sign(direction.x), 0)
+		else:
+			direction_looking = Vector2(0, sign(direction.y))
+	$InteractionArea.position = direction_looking * 20 
 	
+	
+	move_and_slide()
 
 	#lanterna dentro da sala
 	if inside_room and not flashlight.enabled and not test_1:
@@ -58,3 +72,5 @@ func _on_living_room_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		inside_room = false
 		flashlight_timer = 0
+		
+		
---
+++ Scripts/food.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+var was_eaten = false
+
+func interact():
+	
+	was_eaten = true
+	$Sprite2D.hide()
